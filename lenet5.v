2467901ba24fbcbd78131aecc6a885210d061961
wjdtmdals29 <109125304+wjdtmdals29@users.noreply.github.com>
2022-12-08 01:42:38 +0900

    Update lenet5.v

--- lenet5.v
+++ lenet5.v
@@ -1,5 +1,5 @@
 /*******************************************************************************
-Author: SeungminJeong(newb)(Department of Electrical Engineering, Kwanwoon University, Nowon-gu, Seoul, South of Korea)
+Author: SeungminJeong(newb)(Electrical Engineering, Kwanwoon University, South of Korea)
 Associated Filename: lenet5.v
 Purpose: verilog code to lenet5
 Introduction : 28x28x1 input -> 5x5x3 conv1 -> 24x24x3 out -> Relu -> Max -> 14x14x3 -> 3x5x5x3 conv2 -> 8x8x3 out -> Relu -> Max -> 4x4x3 -> 48x10 FC layer -> 0-9 out
